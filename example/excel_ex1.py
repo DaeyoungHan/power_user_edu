4f6644cd96fe3b087030085724df42f4b90c105d
DaeyoungHan <87626398+DaeyoungHan@users.noreply.github.com>
2022-10-21 13:58:18 +0900

    Create excel_ex1.py

---
+++ example/excel_ex1.py
@@ -0,0 +1,15 @@
+# 2022.10 HDY
+# 파워유저교육(파이썬) - 자료형
+
+name = '홍길동'                         # 문자 자료형
+address = '광주광역시 광산구 손재로'     # 문자 자료형
+age = 20                                # 숫자 자료형
+marketing_check = True                  # 불리언 자료형
+
+print(name)
+print(3.14)
+print(name, address, age, marketing_check)
+
+print(1)
+print(False)
+print('안녕하세요')
